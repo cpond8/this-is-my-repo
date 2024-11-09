26504faf650ee1994fa6aaf5b2c2b81153e5d80c
cpond8 <157740615+cpond8@users.noreply.github.com>
2024-11-09 12:04:33 -0800

    Update solutions_i_am_proud_of.py

--- solutions_i_am_proud_of.py
+++ solutions_i_am_proud_of.py
@@ -1,29 +1,24 @@
-# Reverse a string of text.
-def reverse(text: str) -> str:
-    return "" if not text else text[-1] + reverse(text[:-1])
-
-
 # Apply ROT[key] to text.
 def rotate(text: str, key: int) -> str:
     def offset(char: str) -> str:
         base = ord("a") if char.islower() else ord("A")
         return chr((ord(char) - base + key) % 26 + base)
         
-    return "".join([offset(c) if c.isalpha() else c for c in text])
+    return "".join(offset(c) if c.isalpha() else c for c in text)
 
 
 # Convert a number up to 3999 into Roman numerals.
 def roman(num: int) -> str:
-    def convert(n: int, power_of_ten: int) -> str:
-        one, five, ten = ("IVX", "XLC", "CDM", "MMM")[power_of_ten]
-        return (one if n % 5 == 4 else five if n > 4 else "") + (
+    def convert(n: int, pow_of_10: int) -> str:
+        one, five, ten = ("IVX", "XLC", "CDM", "MMM")[pow_of_10]
+        return (
+            one if n % 5 == 4 else five if n > 4 else "") + (
             ten if n == 9 else five if n == 4 else one * (n % 5)
         )
-
+    
     return "".join(
-        map(convert,
-            [int(n) for n in str(num)],
-            range(len(str(num)))[::-1])
+        convert(int(n), i)
+        for n, i in zip(str(num), reversed(range(len(str(num)))))
     )
 
 
@@ -37,18 +32,12 @@ def sublist(list_one, list_two) -> str:
 
 
 # Check the validity of an ISBN.
-def is_valid(isbn_raw: str) -> bool:
-    def clean(isbn: str) -> str:
-        return isbn.replace("-", "").rstrip("X") + ("#" if isbn[-1:] == "X" else "")
-        
-    def to_int(n: str) -> int:
-        return int(n) if n.isdigit() else 10 if n == "#" else 0.1
-        
-    def checksum(isbn: list[int]) -> int:
-        return sum([i * n for i, n in zip(isbn, range(10, 0, -1))])
-
-    isbn_int = [to_int(n) for n in clean(isbn_raw)]
-    return len(isbn_int) == 10 and checksum(isbn_int) % 11 == 0
+def is_valid(isbn: str) -> bool:
+    isbn = {
+        i: 10 if (n == "X" and i == 1) else int(n) if n.isdigit() else 0.1
+        for i, n in zip(range(10, -1, -1), isbn.replace("-", ""))
+    }
+    return len(isbn) == 10 and sum(i * n for i, n in isbn.items()) % 11 == 0
 
 
 # Given an array of integers nums, return the length of
@@ -74,31 +63,13 @@ def miles_to_golden_km(miles: int) -> int:
     return kilometers
 
 
-# Given an m x n matrix of integers as a list of row vectors, return a list of every element in the matrix in spiral order.
-def spiral_order(matrix: list[list[int]]) -> list[int]:
-    x0, y0 = 0, 0
-    xf = len(matrix[0]) - 1
-    yf = len(matrix) - 1
-    spiral = []
+# Apply the Atbash cipher to a given string, and return the encypted text
+# divided into blocks of 5 characters. Leave numerals unchanged and
+# ignore any non-alphanumeric characters.
+atbash = str.maketrans(abc := "abcdefghijklmnopqrstuvwxyz", abc[::-1], " ")
 
-    while x0 <= xf and y0 <= yf:
-        # top side rightward
-        for x in range(x0, xf + 1):
-            spiral.append(matrix[y0][x])
-        y0 += 1
-        # right side downward
-        for y in range(y0, yf + 1):
-            spiral.append(matrix[y][xf])
-        xf -= 1
-        # bottom side leftward
-        if y0 <= yf:
-            for x in range(xf, x0 - 1, -1):
-                spiral.append(matrix[yf][x])
-            yf -= 1
-        # left side upward
-        if x0 <= xf:
-            for y in range(yf, y0 - 1, -1):
-                spiral.append(matrix[y][x0])
-            x0 += 1
-
-    return spiral
+def encode(txt: str) -> str:
+    ciph = "".join(
+        c.lower().translate(atbash) for c in txt if c.isalnum()
+    )
+    return " ".join(ciph[i:i+5] for i in range(0, len(ciph), 5))
